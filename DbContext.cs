615883c980a40ae1ac1955d57dd0a76064200631
Kate Vyshenskaia <lmaoyyyya@gmail.com>
2024-04-10 10:19:57 +0500

    dbcontext
    dbcontexxxxxt

---
+++ DbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace WpfApp1
+{
+    public class KotyaDbContext : DbContext
+    {
+        public KotyaDbContext(DbContextOptions<KotyaDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Server=localhost; port=5432 ; user id =postgres ; password =kotya; database = ft_bd_1; ");
+            }
+        }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,55 +12,73 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore; // фикс подчеркивания говна
 
 namespace WpfApp1
 {
 
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        string vStrConnecion = "Server=localhost; port=5432 ; user id =postgres ; password =kotya; database = ft_bd_1; ";
+
         public MainWindow()
         {
             InitializeComponent();
-        }
-        string vStrConnecion = "Server=localhost; port=5432 ; user id =postgres ; password =kotya; database = ft_bd_1; ";
-        NpgsqlConnection vCon;
-        NpgsqlCommand vCmd;
-
-        private void connection()
-        {
-            vCon = new NpgsqlConnection();
-            vCon.ConnectionString = vStrConnecion;
-            if (vCon.State == ConnectionState.Closed) 
-            {
-                vCon.Open();
-            }
-            vCon.Open();
         }
 
         public DataTable GetData(string sql)
         {
             DataTable dt = new DataTable();
-            connection();
-            vCmd = new NpgsqlCommand();
-            vCmd.Connection = vCon;
-            vCmd.CommandText=sql;
-            NpgsqlDataReader dr = vCmd.ExecuteReader();
-            dt.Load(dr);
-
+            using (NpgsqlConnection vCon = new NpgsqlConnection(vStrConnecion))
+            {
+                vCon.Open();
+                using (NpgsqlCommand vCmd = new NpgsqlCommand(sql, vCon))
+                {
+                    using (NpgsqlDataReader dr = vCmd.ExecuteReader())
+                    {
+                        dt.Load(dr);
+                    }
+                }
+            }
             return dt;
         }
-        private void MainWindow_Load(object sender, EventArgs e)
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            connection();
+            DataTable dtgetdata = GetData("select * from products;");
+
+            dgData.ItemsSource = dtgetdata.DefaultView;
         }
+
+
+        private void DeleteSelectedRow()
+        {
+            DataRowView selectedRow = (DataRowView)dgData.SelectedItem;
+            if (selectedRow != null)
+            {
+                int id = Convert.ToInt32(selectedRow["id"]); 
+                string deleteQuery = $"DELETE FROM products WHERE id = {id}";
+
+                using (NpgsqlConnection vCon = new NpgsqlConnection(vStrConnecion))
+                {
+                    vCon.Open();
+                    using (NpgsqlCommand command = new NpgsqlCommand(deleteQuery, vCon))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                // Обновляем DataGrid после удаления строки
+                DataTable updatedData = GetData("SELECT * FROM products");
+                dgData.ItemsSource = updatedData.DefaultView;
+            }
+        }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            DataTable dtgetdata = new DataTable();
-            dtgetdata = GetData("select * from products;");
+            DeleteSelectedRow();
         }
+
     }
 }
